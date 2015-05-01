a840785bb95a6743ad5403cf02e46727450a8d2a
Trent Clarke <trent.clarke@gmail.com>
2015-05-01 21:12:58 +1000

    Added simple suffix tree implementation

---
+++ gst/gst.go
@@ -0,0 +1,235 @@
+package gst
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	/// The length value indicating "as long as the string"
+	inf = -1
+)
+
+/// node represents a chunk of text inside the suffix tree. It doesn't store
+/// the text itself, it only stores pointers to the text in an string.
+type node struct {
+	parent   *node
+	suffix   *node
+	offset   int
+	length   int
+	children map[rune]*node
+}
+
+/// newNode creates and initialises a node in its defauts state: starting at a
+/// given offset and extending for the remainder or the internal text
+func newNode(start int, parent *node) *node {
+	return &node{
+		parent:   parent,
+		offset:   start,
+		suffix:   nil,
+		length:   inf,
+		children: make(map[rune]*node),
+	}
+}
+
+/// split splits a node into two parts
+func (self *node) split(text []rune, i, length int) (*node, *node, *node) {
+	oldParent := self.parent
+	newLeaf := newNode(i, nil)
+	newParent := &node{
+		parent: oldParent,
+		suffix: self.suffix,
+		offset: self.offset,
+		length: length,
+		children: map[rune]*node{
+			text[self.offset+1]: self,
+			text[i]:             newLeaf},
+	}
+
+	newLeaf.parent = newParent
+
+	oldParent.children[text[self.offset]] = newParent
+	self.parent = newParent
+	self.offset += length
+	if self.length != inf {
+		self.length -= length
+	}
+	self.suffix = nil
+
+	return newParent, self, newLeaf
+}
+
+/// A key/value pair use when iterating over the children of a node
+type child struct {
+	index rune
+	n     *node
+}
+
+/// childNodes returns a slice of key/value pairs, representing the
+/// child suffixes of the node, and the indices used to address them.
+func (self *node) childNodes() []child {
+	result := make([]child, len(self.children))
+	i := 0
+	for k, v := range self.children {
+		result[i] = child{index: k, n: v}
+		i++
+	}
+	return result
+}
+
+/// id generates an ID string for the node.
+func (self *node) id() string {
+	return fmt.Sprintf("%p", self)
+}
+
+/// label() generates a label for the node, representing its text value
+func (self *node) label(text []rune) string {
+	last := len(text)
+	if self.length != -1 {
+		last = self.offset + self.length
+	}
+	if self.offset >= len(text) {
+		panic("oob")
+	}
+	return string(text[self.offset:last])
+}
+
+func (self *node) isShorterThan(n int) bool {
+	if self.length != inf {
+		return self.offset+self.length <= n
+	}
+	return false
+}
+
+///
+type SuffixTree struct {
+	root *node
+	text []rune
+}
+
+/// Creates a new suffix treen and initialises it from the supplied string.
+func New(s string) SuffixTree {
+	tree := SuffixTree{
+		root: newNode(-1, nil),
+		text: make([]rune, 0),
+	}
+	tree.Insert(s)
+	return tree
+}
+
+type activePointState struct {
+	node   *node
+	edge   rune
+	length int
+}
+
+func (self *activePointState) edgeTarget() *node {
+	if result, ok := self.node.children[self.edge]; ok {
+		return result
+	}
+	return nil
+}
+
+func (self *activePointState) slide(child *node, text []rune) bool {
+	if child.length != inf && self.length >= child.length {
+		self.length -= child.length
+		self.edge = text[child.offset]
+		self.node = child
+		return true
+	}
+	return false
+}
+
+/// Generates a suffix link between a the nodes if prevNode is not nil.
+func link(prev, next *node) *node {
+	if prev != nil {
+		prev.suffix = next
+	}
+	return next
+}
+
+/// Insert inserts a new string into the suffix tree.
+func (self *SuffixTree) Insert(s string) {
+	active := activePointState{self.root, '\x00', 0}
+	remainder := 0
+	for _, c := range s {
+		i := len(self.text)
+		self.text = append(self.text, c)
+		remainder++
+
+		var prevNode *node = nil
+
+		for remainder > 0 {
+			if active.length == 0 {
+				active.edge = c
+			}
+
+			activeChild, ok := active.node.children[active.edge]
+			if !ok {
+				newChild := newNode(i, active.node)
+				active.node.children[active.edge] = newChild
+				prevNode = link(prevNode, newChild)
+			} else {
+				if active.slide(activeChild, self.text) {
+					continue
+				}
+
+				if self.text[activeChild.offset+active.length] == c {
+					active.length++
+					prevNode = link(prevNode, active.node)
+					break
+				} else {
+					np, _, _ := activeChild.split(self.text, i, active.length)
+					prevNode = link(prevNode, np)
+				}
+			}
+			remainder--
+
+			if active.node == self.root && active.length > 0 {
+				active.length--
+				active.edge = self.text[i-active.length]
+			} else {
+				if active.node.suffix != nil {
+					active.node = active.node.suffix
+				} else {
+					active.node = self.root
+				}
+			}
+		}
+	}
+}
+
+/// dumpTree writes the tree out to a dot-formatted file  for diagnstic
+/// purposes.
+func (self *SuffixTree) dumpTree(filename string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic("Failed to open graph output file")
+	}
+	defer file.Close()
+
+	file.WriteString("digraph G {\n")
+	defer file.WriteString("}")
+
+	file.WriteString(fmt.Sprintf("\"%p\" [label=\"root\"]\n", self.root))
+
+	queue := self.root.childNodes()
+	for len(queue) > 0 {
+		n := queue[0].n
+		x := queue[0].index
+		if x == '\x00' {
+			x = '?'
+		}
+		queue = queue[1:]
+
+		label := n.label(self.text)
+		file.WriteString(fmt.Sprintf("\"%p\" [label=\"%s\"]\n", n, label))
+		file.WriteString(fmt.Sprintf("\"%p\" -> \"%p\" [label=\"%c\"]\n", n.parent, n, byte(x)))
+
+		if n.suffix != nil {
+			file.WriteString(fmt.Sprintf("\"%p\" -> \"%p\" [style=\"dotted\"]\n", n, n.suffix))
+		}
+
+		queue = append(queue, n.childNodes()...)
+	}
+}
---
+++ gst/gst_test.go
@@ -0,0 +1,47 @@
+package gst
+
+import (
+	"testing"
+)
+
+func Test_Init(t *testing.T) {
+	//New("abcabxabcd")
+	New("cdddcdc")
+}
+
+func decode(s string) []rune {
+	result := make([]rune, 0, len(s))
+	for _, ch := range s {
+		result = append(result, ch)
+	}
+	return result
+}
+
+func Test_SplittingNode(t *testing.T) {
+	text := decode("abcabx")
+
+	parent := newNode(-1, nil)
+	n := newNode(0, parent)
+	parent.children['a'] = n
+
+	newParent, nn, newChild := n.split(text, 2, 2)
+
+	if parent.children['a'] != newParent {
+		t.Errorf("Expected new parent to be a child or parent")
+	}
+
+	if nn != n {
+		t.Errorf("Expected nn to be the original node")
+	}
+
+	_ = newChild
+
+	if newParent.length != 2 {
+		t.Errorf("Expected new parent to have length (2, got %d", newParent.length)
+	}
+
+	if newChild.length != inf {
+		t.Errorf("Expected new child to have length (inf), got %d", newChild.length)
+	}
+
+}
